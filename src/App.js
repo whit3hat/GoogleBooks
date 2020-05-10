57a2b8f5f0ef895d76f0bd12b1190bc696797e12
Spencer Wood <spencer.wood@me.com>
2020-05-09 22:19:48 -0500

    added the url and started the function for the google books api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Books from './pages/Books';
 import Nav from './components/Nav';
-const express = require('express');
+const axios = require('axios').default;
 
+// URL for Google Books
+const URL = 'https://www.googleapis.com/books/v1/volumes?q='
 
+//function to query the google books api
+axios.get(`${URL}=handleResponse`)
+      .then(function (res) {
+        console.log(res)
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+      .then(function () {
 
+      });
 
 function App() {
   return (
