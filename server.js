0c7d7352aebcbf282b975e4d23e986e84e845f26
Spencer Wood <spencer.wood@me.com>
2020-08-21 21:57:33 -0500

    finished the models for the DB info and uncommented out the middleware on the server.js file

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// // Middleware
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Static for Heroku or other platform
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-// }
+Static for Heroku or other platform
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
-// // Add routes, API and view
-// app.use(routes);
+// Add routes, API and view
+app.use(routes);
 
-// // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gbooks');
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gbooks');
 
 // Start the API server
 app.listen(PORT, function() {
