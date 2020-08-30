67713f15d02a292c40463072480ae6cbc561c381
Spencer Wood <spencer.wood@me.com>
2020-08-29 20:48:40 -0500

    added basic variables and function to the gbooks api function

--- client/src/utils/Gbooks.js
+++ client/src/utils/Gbooks.js
@@ -1,2 +1,16 @@
 import axios from 'axios';
+import { query } from 'express';
 
+const URL = '';
+const API_KEY = '';
+
+export const googleBooks = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+
+        }
+    });
+
+    return data;
+}
